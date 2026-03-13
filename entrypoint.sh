ca07f6ce1cb9b6bc3bca227a4112a78ea19a20d5
yuutachichao <147903479+yuutachichao@users.noreply.github.com>
2026-03-13 14:55:44 +0800

    Set permissions for copied configuration files
    Add permission change for copied files in entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,9 +4,10 @@ set -e
 mkdir -p /root/.config/gws /data/gws
 
 for f in .encryption_key client_secret.json credentials.enc token_cache.json; do
-  if [ -f "/data/gws/$f" ]; then
-    cp "/data/gws/$f" "/root/.config/gws/$f"
-  fi
+if [ -f "/data/gws/$f" ]; then
+cp "/data/gws/$f" "/root/.config/gws/$f"
+chmod 600 "/root/.config/gws/$f"
+fi
 done
 
 exec "$@"
